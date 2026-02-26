c806b19afbb88d5ee6f86f86ffce91f5edb9cb61
Nadia <tigresitacoria536@gmail.com>
2026-02-26 13:04:15 -0600

    nuevo

--- primitivas_dibujo.py
+++ primitivas_dibujo.py
@@ -1,22 +1,32 @@
 import cv2 as cv
-import numpy as np 
+import numpy as np
 
-img = np.ones((500,500), np.uint8)*150 
-cv.rectangle(img, (10,10), (200,100), (34,56,100), -1)
-cv.circle(img, (255,255), 1, (23, 43, 144), -1 )
-cv.line(img, (255,255), (200,100), (23, 244, 144), 4)
-for i in range(400):
-    cv.circle(img, (i,i), i , (255, 0, 0), -1 )
-    cv.rectangle(img, (10+i,10), (200,100), (34,56,100), -1)
+width, height = 800, 500
 
-    cv.imshow('img', img)
-    #img = np.ones((500,500,3), np.uint8)*150 
-    cv.waitKey(30)
+for i in range(0, 400, 5): # El carro se moverá de 5 en 5 píxeles
+    img = np.ones((height, width, 3), np.uint8) * 200
     
+    
+    # Ruedas
+    cv.circle(img, (150 + i, 350), 30, (0, 0, 0), -1) # Rueda trasera
+    cv.circle(img, (350 + i, 350), 30, (0, 0, 0), -1) # Rueda delantera
+    
+    # Carroceria 
+    cv.rectangle(img, (100 + i, 250), (400 + i, 330), (50, 50, 255), -1) 
+    
+    # Cabina 
+    cv.rectangle(img, (150 + i, 180), (320 + i, 250), (100, 100, 255), -1)
+    
+    # Ventana 
+    cv.rectangle(img, (170 + i, 200), (230 + i, 240), (255, 255, 255), -1)
+    cv.rectangle(img, (250 + i, 200), (300 + i, 240), (255, 255, 255), -1)
 
+    # Suelo 
+    cv.line(img, (0, 380), (width, 380), (50, 50, 50), 2)
 
+    cv.imshow('Animacion de Carro', img)
+ 
+    if cv.waitKey(30) & 0xFF == ord('q'):
+        break
 
-
-cv.imshow('img', img)
-cv.waitKey(0)
 cv.destroyAllWindows()
